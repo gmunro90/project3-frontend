d8e065fc8119baf913d9fecb6ef20ef89c3d1cc6
colinchadwick1 <colinchadwick@hotmail.co.uk>
2021-12-09 15:11:39 +0100

    sportDetails

--- src/pages/SportDetailsPage.js
+++ src/pages/SportDetailsPage.js
@@ -3,55 +3,49 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AddTask from "../components/AddTask";
 import { useParams } from "react-router";
-import events from "../events.json"
-
+import events from "../events.json";
 
 import TaskCard from "../components/TaskCard";
 
 const API_URI = process.env.REACT_APP_API_URI;
 
 function SportDetailsPage(props) {
-  const [sport, setSport] = useState([]);
-  const [isLoading, setIsLoading] = useState(true)
-  const {id: sportId} = useParams()
+  const [sport, setSport] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const { id: sportId } = useParams();
 
   // const getProject = () => {
   //   // Get the token from the localStorage
   //   const storedToken = localStorage.getItem("authToken");
 
   //   // Send the token through the request "Authorization" Headers
- 
 
   useEffect(() => {
-    const filteredSport = events.filter(event=> event._id === sportId)
-    setSport(filteredSport)
-    setIsLoading(false)
-
-
-  }, [])
-  console.log(sport)
+    const filteredSport = events.filter((event) => event._id === sportId);
+    setSport(filteredSport[0]);
+    setIsLoading(false);
+  }, []);
+  console.log(sport);
 
   return (
-    {isLoading ? '<p> Data is loading </p>' : (
-      <div className="SportDetails">
-      
-      <>
-        <h1>{sportId}</h1>
-        <p>{sport[0].sport}</p>
-        <p>Location: {sport[0].location}</p>
-        <p>Players: {sport[0].players}</p>
-        <p>Time: {sport.time}</p>
-        <p>Price: {sport.price}</p>
-      </>
-    
+    <div className="SportDetails">
+      {isLoading ? (
+        <p> Data is loading </p>
+      ) : (
+        <>
+          <h1>{sportId}</h1>
+          <p>{sport.sport}</p>
+          <p>Location: {sport.location}</p>
+          <p>Players: {sport.players}</p>
+          <p>Time: {sport.time}</p>
+          <p>Price: {sport.price}</p>
 
-    <Link to={`/`}>
-      <button>Home</button>
-    </Link>
-  </div>
-
-    )}
-    
+          <Link to={`/`}>
+            <button>Home</button>
+          </Link>
+        </>
+      )}
+    </div>
   );
 }
 
