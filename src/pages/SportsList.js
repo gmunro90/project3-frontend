c806c84ecb5fe7b1b4b3ee26ab59bc9f76225ed8
colinchadwick1 <colinchadwick@hotmail.co.uk>
2021-12-13 15:42:42 +0100

    h

--- src/pages/SportsList.js
+++ src/pages/SportsList.js
@@ -73,7 +73,7 @@ function SportsListPage() {
                 <Link to={`sports/${sport._id}`}>
                   <h2>{sport.sport} {sport.venue.location.barrio} </h2>
                 </Link>
-                <p>Players: {sport.players.map((player)=>player.name)}</p>
+                <p>Players: {sport.players}</p>
                 <p>{sport.venue.location.barrio}</p>
               </>
             );
