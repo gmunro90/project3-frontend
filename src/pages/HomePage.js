70b335dc75730f4a78c8bd83cd22f8afade74a91
Greg Munro <gregmunro90@gmail.com>
2021-12-13 18:45:44 +0100

    pushpush

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -25,9 +25,8 @@ function HomePage() {
 
       <div>
         <Link to={`/search?sport=${sport[0]}`}>
-          <svg
-            width="50"
-            height="50"
+          <svg className="w-10"
+            
             viewBox="0 0 62 62"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
